e687886d2003c1f751371336a4c2350a1a1b9517
michaldobiezynski <dobiezynski@gmail.com>
2020-05-24 20:52:35 +0100

    updated server.js for serving dist folder

--- server.js
+++ server.js
@@ -3,7 +3,8 @@ var path = require("path");
 var serveStatic = require("serve-static");
 
 var app = express();
-app.use(serveStatic(path.join(__dirname, "dist")));
+var distDir = __dirname + "/dist/";
+app.use(express.static(distDir));
 
 var port = process.env.PORT || 8000;
 app.listen(port);
