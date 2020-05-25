669886517cc3235134b81d17093b60975790f1fd
michaldobiezynski <dobiezynski@gmail.com>
2020-05-25 09:58:05 +0100

    using enviromental variables

--- src/actions/index.js
+++ src/actions/index.js
@@ -4,7 +4,7 @@ import { AUTH_USER, AUTH_ERROR } from "./types";
 export const signup = (formProps, callback) => async (dispatch) => {
   try {
     const response = await axios.post(
-      "http://localhost:5000/signup",
+      process.env.REACT_APP_API_URL + "/signup",
       formProps,
     );
 
@@ -19,7 +19,7 @@ export const signup = (formProps, callback) => async (dispatch) => {
 export const signin = (formProps, callback) => async (dispatch) => {
   try {
     const response = await axios.post(
-      "http://localhost:5000/signin",
+      process.env.REACT_APP_API_URL + "/signin",
       formProps,
     );
 
